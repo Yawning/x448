63bb4f5daf91907e03d855152f62143e1604d410
Yawning Angel <yawning@schwanenlied.me>
2015-12-25 02:57:21 +0000

    Update the import annotation.

--- x448.go
+++ x448.go
@@ -26,7 +26,7 @@
 // elliptic curve known as curve448.
 //
 // See https://tools.ietf.org/html/draft-irtf-cfrg-curves-11
-package x448 // import "git.schwanenlied.me/yawning/x448"
+package x448 // import "git.schwanenlied.me/yawning/x448.git"
 
 const (
 	x448Bytes = 56
